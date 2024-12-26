bd08d484c6604b3341b0bd586e5b368a8ca3434c
Marcos Alves <mrcaurelio95@gmail.com>
2024-12-25 21:35:22 -0300

    Custom Hook
    Criando custom hook (useFetch)

--- project-react-7-req-http/src/App.jsx
+++ project-react-7-req-http/src/App.jsx
@@ -1,22 +1,32 @@
 import './App.css'
 import { useState, useEffect } from "react"
 
+//4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
 const url = "http://localhost:3000/products"
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  //4 - custom hook
+  const { data:items } = useFetch(url)
+  
+  
+  
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   // 1-resgatando dados
-  useEffect(() => {
-    async function fetchProducts() {
-
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //    async function fetchProducts() {
+  //
+  //      const res = await fetch(url);
+  //      const data = await res.json();
+  //      setProducts(data);
+  //    }
+  //    fetchProducts();
+  //  }, []);
 
   //2-add de produtos
   const handleSubmit = async (e) => {
@@ -48,7 +58,7 @@ function App() {
     <>
       <h1>Lista de produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>{product.name} - R$ {product.price}</li>
         ))}
       </ul>
