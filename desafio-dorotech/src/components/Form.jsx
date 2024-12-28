a1d40db7a63302b378af2dcb5e988ef4c33156aa
Marcos Alves <mrcaurelio95@gmail.com>
2024-12-28 17:19:43 -0300

    commit-28-12-1719

--- desafio-dorotech/src/components/Form.jsx
+++ desafio-dorotech/src/components/Form.jsx
@@ -1,32 +1,15 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import './Form.css';
 
-const Form = () => {
-  const [formData, setFormData] = useState({
-    searchText: '',
-    status: {
-      vivo: false,
-      morto: false,
-      desconhecido: false,
-    },
-  });
+const Form = ({search, setSearch }) => {
 
   const handleInputChange = (e) => {
-    setFormData({ ...formData, searchText: e.target.value });
-  };
-
-  const handleCheckboxChange = (e) => {
-    const { name, checked } = e.target;
-    setFormData({
-      ...formData,
-      status: { ...formData.status, [name]: checked },
-    });
+    setSearch(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form data:', formData); // Para fins de teste
-    alert(`Buscando: ${formData.searchText}\nStatus: ${JSON.stringify(formData.status)}`);
+
   };
 
   return (
@@ -37,44 +20,13 @@ const Form = () => {
           type="text"
           id="searchText"
           name="searchText"
-          value={formData.searchText}
+          value={search}
           onChange={handleInputChange}
           placeholder="Digite o nome"
         />
       </div>
 
-      <div className="form-group">
-        <label>Status:</label>
-        <div className="checkbox-group">
-          <label>
-            <input
-              type="checkbox"
-              name="vivo"
-              checked={formData.status.vivo}
-              onChange={handleCheckboxChange}
-            />
-            Vivo
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="morto"
-              checked={formData.status.morto}
-              onChange={handleCheckboxChange}
-            />
-            Morto
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="desconhecido"
-              checked={formData.status.desconhecido}
-              onChange={handleCheckboxChange}
-            />
-            Desconhecido
-          </label>
-        </div>
-      </div>
+
 
       <button type="submit">Enviar</button>
     </form>
