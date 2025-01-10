ef6549f3c1bccdebc91d114feae8a301c79dd606
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-09 22:22:08 -0300

    aula react-router

--- project-react-8/aula8/src/App.jsx
+++ project-react-8/aula8/src/App.jsx
@@ -1,9 +1,23 @@
+//1-react router dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Navbar from './components/Navbar/Navbar'
+
 function App() {
 
   return (
-    <>
-      <h1>React Chamaa</h1>
-    </>
+    <div className='App'>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
