ad241ba06ed4a80e5f92edf65234790e00d44bb0
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-12 12:30:02 -0300

    2-criar provider

--- project-react-9/modulo-9/src/context/CounterContext.jsx
+++ project-react-9/modulo-9/src/context/CounterContext.jsx
@@ -1,4 +1,18 @@
 //1 - criar context
 import { createContext, useState } from "react";
 
-export default CounterContext = createContext();
+export const CounterContext = createContext();
+
+
+//2 - Criar provider
+export const CounterContextProvider = ({ children }) => {
+
+    const [counter, setCounter] = useState(5);
+
+    return (
+        <CounterContext.Provider value={{ counter, setCounter }}>
+            {children}
+        </CounterContext.Provider>
+    )
+
+}
