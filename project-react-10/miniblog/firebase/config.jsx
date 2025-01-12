0d3ee97a18e5136cbbbf82cd1b96964effe70157
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-12 17:26:17 -0300

    Create config.jsx

---
+++ project-react-10/miniblog/firebase/config.jsx
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBYIqdz8hYh8sERQTKzqVr_14CKxqzTC3o",
+    authDomain: "miniblog-3b987.firebaseapp.com",
+    projectId: "miniblog-3b987",
+    storageBucket: "miniblog-3b987.firebasestorage.app",
+    messagingSenderId: "222528174170",
+    appId: "1:222528174170:web:f91fd0276f11ca783cc063"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db };
