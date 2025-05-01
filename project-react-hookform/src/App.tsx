b4a6a55f687d173d749b6d0a5280eff749a432d1
Marcos Alves <mrcaurelio95@gmail.com>
2025-05-01 20:08:31 -0300

    feat:project-react-hookform
    -estudando react-hook-form com Yup

--- project-react-hookform/src/App.tsx
+++ project-react-hookform/src/App.tsx
@@ -3,7 +3,6 @@ import Formulario from "./components/Formulario/Formulario";
 function App() {
   return (
     <>
-      <h1>Olá form</h1>
       <Formulario />
     </>
   );
--- project-react-hookform/src/components/Formulario/Formulario.tsx
+++ project-react-hookform/src/components/Formulario/Formulario.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
 interface IForm {
   name: string;
@@ -7,49 +12,71 @@ interface IForm {
   confirmPassword: string;
 }
 
+const schema = Yup.object().shape({
+  name: Yup.string().required("Campo obrigatório"),
+  email: Yup.string().required("Campo obrigatório"),
+  password: Yup.string()
+    .min(6, "Precisa ter no mínimo 6 caracteres")
+    .required("Campo obrigatório"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "As senhas precisam ser iguais")
+    .required("Campo obrigatório"),
+});
+
 const Formulario = () => {
+  const [value, setValue] = useState<string | undefined>();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IForm>();
+  } = useForm({ mode: "all", resolver: yupResolver(schema) });
 
   console.log({ errors });
 
   const handleSubmitData = (data: IForm) => {
-    alert(`${data.name}`);
+    console.log(data);
+    setValue(JSON.stringify(data));
   };
 
   return (
-    <form>
-      <h1>Cadastro</h1>
+    <form
+      className="flex flex-col justify-center items-center gap-2 mt-4"
+      onSubmit={handleSubmit(handleSubmitData)}
+    >
+      <h1 className="font-bold text-3xl">Cadastro</h1>
       <input
-        {...register("name", { required: true })}
+        className="border-1 rounded-md p-1"
+        {...register("name")}
         type="text"
         placeholder="Digite seu nome"
       />
-      {errors?.name?.type === "required" && <p>Name is required</p>}
+      {errors.name && <p>{errors.name.message}</p>}
       <input
-        {...register("email", { required: true })}
-        type="text"
+        className="border-1 rounded-md p-1"
+        {...register("email")}
+        type="email"
         placeholder="Digite seu e-mail"
       />
       <input
-        {...register("password", { minLength: 7, required: true })}
+        className="border-1 rounded-md p-1"
+        {...register("password")}
         type="password"
         placeholder="senha"
       />
-      {errors?.password?.type === "required" && <p>Password is required</p>}
-      {errors?.password?.type === "minLength" && (
-        <p>Password must have 7 characters</p>
-      )}
+
       <input
+        className="border-1 rounded-md p-1"
         {...register("confirmPassword")}
         type="password"
         placeholder="confirme a senha"
       />
+      {errors.confirmPassword?.type === "oneOf" && (<p>{errors.confirmPassword.message}</p>)}
 
-      <button onClick={handleSubmit(handleSubmitData)}>Reset</button>
+      <button className="font-bold rounded-md p-2 bg-emerald-500 hover:bg-emerald-600 text-white">
+        Reset
+      </button>
+      <p className="w-">{value}</p>
     </form>
   );
 };
